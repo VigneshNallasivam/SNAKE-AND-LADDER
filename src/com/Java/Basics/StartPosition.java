86187bacee8b69854cc77bf0b54fbba11944c627
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-25 22:03:58 +0530

    Added : UC-1 Printing the Position of User

---
+++ src/com/Java/Basics/StartPosition.java
@@ -0,0 +1,13 @@
+package com.Java.Basics;
+
+public class StartPosition 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("WELCOME TO SNAKE & LADDER GAME");
+		int userPosition = 1;
+		System.out.println("******Game Starts*******");
+		System.out.println("Initial User Position : " +userPosition);
+
+	}
+}
