1a2a3ffc9a14f62498d0fb52bc7ab20be03dbdbc
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-25 21:47:07 +0530

    Added : Printing the Welcome Message to the Snake & Ladder Program

---
+++ src/com/Java/Basics/Display.java
@@ -0,0 +1,12 @@
+package com.Java.Basics;
+
+public class Display 
+{
+
+	public static void main(String[] args) 
+	{
+	  System.out.println("Welcome To Snake And Ladder Program Computation");
+
+	}
+
+}
